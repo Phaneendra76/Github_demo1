2e19990502c49746e4c880dc08039672f9f8c17c
Phaneendra79 <155842268+Phaneendra76@users.noreply.github.com>
2024-01-07 13:51:07 +0530

    Javacode

---
+++ src/Class_1.java
@@ -0,0 +1,14 @@
+
+public class Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		System.out.println("Test 3");
+		System.out.println("Test 3");
+
+	}
+
+}
